b67872523eb1a4b16b4be4b2c8a96fbe88c2f5cd
yu <yul1024@outlook.com>
2026-06-04 19:47:29 +0800

    refactor: move

--- modules/monitor/nohup_wrapper.py
+++ modules/monitor/nohup_wrapper.py
@@ -1,32 +1,33 @@
 """
 Sources:
-    https://github.com/yuliu625/Yu-CLI-Wrapper/monitor/nohup_wrapper.py
+    https://github.com/yuliu625/Yu-CLI-Wrapper/blob/main/modules/monitor/nohup_wrapper.py
 
 References:
     - https://www.gnu.org/software/coreutils/manual/html_node/nohup-invocation.html
     - https://man7.org/linux/man-pages/man1/nohup.1.html
 
 Synopsis:
-    nohup命令的参数转义和运行封装。
+    nohup 命令的参数转义和运行封装。
 
 Notes:
-    nohup使用频率较高，但是其参数和符号可读性很差。我转义和封装了所有nohup的所有可选配置参数。
+    nohup 使用频率较高，但是其参数和符号可读性很差。我转义和封装了所有 nohup 的所有可选配置参数。
 
     更优选择:
-        - subprocess.Popen: nohup的本质本身就已经是一个wrapper:
-            nohup进行:
-                - 信号处理: 拦截SIGHUP(Signal Hang Up)
+        - subprocess.Popen: nohup 的本质本身就已经是一个 wrapper :
+            nohup 进行:
+                - 信号处理: 拦截 SIGHUP (Signal Hang Up) 。
                 - 重定向: 输出脱离终端，指向目标文件。
             这使得:
-                - 这2个系统调用不如python原生系统调用更加干净可控。
-                - wrapper上的wrapper很奇怪。
-        - screen / tmux: 仅shell条件下，linux其他命令。更易进行检查和控制。
+                - 这 2 个系统调用不如 python 原生系统调用更加干净可控。
+                - wrapper 上的 wrapper 很奇怪。
+        - screen / tmux: 仅 shell 条件下，linux 其他命令。更易进行检查和控制。
 
     停止维护说明:
-        在可以运行python的机器上，该方法完全被纯python系统控制取代。当前方法仅进行存档，不再维护。
+        在可以运行 python 的机器上，该方法完全被纯 python 系统控制取代。当前方法仅进行存档，不再维护。
 """
 
 from __future__ import annotations
+from loguru import logger
 
 import subprocess
 
@@ -36,10 +37,10 @@ from typing import TYPE_CHECKING, Sequence
 
 class NoHupWrapper:
     """
-    nohup参数转义，运行封装工具。
+    nohup 参数转义，运行封装工具。
 
     对照拆解:
-        - > | >> : shell重定向指令。这是shell的功能。
+        - > | >> : shell重定向指令。这是 shell 的功能。
         - 2>&1 : 文件描述符绑定。
         - & : 任务控制符。
     """
@@ -51,7 +52,7 @@ class NoHupWrapper:
         is_dry_run: bool,
     ) -> str | subprocess.Popen[bytes]:
         """
-        使用 nohup 运行原始的shell命令。
+        使用 nohup 运行原始的 shell 命令。
 
         Args:
             command_args (Sequence[str]): 原始的子命令。
@@ -66,7 +67,7 @@ class NoHupWrapper:
                 - str: dry run下的 shell command 字符串。
                 - subprocess.Popen[bytes]: subprocess.Popen 控制对象。
         """
-        # 由于nohup的特殊符号，命令启动需要以str进行。
+        # 由于 nohup 的特殊符号，命令启动需要以 str 进行。
         command_str = "nohup " + " ".join(command_args)
         # 选择追加模式或覆盖模式。
         redirect_operator = '>>' if is_log_append else '>'
