6221cd8e47920ade2fa2d8276061d5e5d02ba792
yu <yul1024@outlook.com>
2026-06-19 14:19:55 +0800

    refactor: rename and move

--- src/monitor/command_runner.py
+++ src/monitor/command_runner.py
@@ -1,9 +1,9 @@
 """
 Sources:
-    https://github.com/yuliu625/Yu-CLI-Wrapper/blob/main/modules/monitor/run_background.py
+    https://github.com/yuliu625/Yu-CLI-Wrapper/blob/main/src/monitor/run_background.py
 
 References:
-    https://github.com/yuliu625/Yu-CLI-Wrapper/blob/main/modules/monitor/command_runner.py
+    https://github.com/yuliu625/Yu-CLI-Wrapper/blob/main/src/monitor/command_runner.py
 
 Synopsis:
     python 原生启动程序方法。
